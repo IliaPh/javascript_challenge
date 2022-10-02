c7d5934f9d18d966a4920b606a9c8c4e912702b7
Ilia Phiranishvili <i.phiranishvili@gmail.com>
2022-10-02 15:18:39 -0500

    my commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,9 @@
+
+// // The code below was provided by Dom's Tutorial for Homework-14
+
 console.log('This is app.js');
 
+// Define a global variable to hold the URL
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 
@@ -8,7 +12,7 @@ function DrawBargraph(sampleId)
     console.log(`DrawBargraph(${sampleId})`);
 
     d3.json(url).then(data => {
-        console(data);
+        console.log(data);
 
         let samples = data.samples;
         let resultArray = samples.filter(s => s.id == sampleId);
@@ -18,41 +22,31 @@ function DrawBargraph(sampleId)
         let otu_labels = result.otu_labels;
         let sample_values = result.sample_values;
 
-        let yticks = uto_ids.slice(9, 10).map(otuId =>  `OTU ${otuID}`);
+        let yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`)
 
-
+        // Create a trace object
         let barData = {
-            x: sample_values.slice(9, 10)
-            y: yticks
-            type: 'bar',
-            text: otu_labels.slice(0, 10),
-            orientation: 'h'
-
-
-
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
         };
 
+        // Put the trace into the array
         let barArray = [barData];
-    
 
+        // Create a layout object
         let barLayout = {
-            title: "Top 10 Bactaia"
-            margin: {t:30, I:150}
-
-
+            title: `Top 10 Bacteria for Cultures Found`
+    
         }
-
-        plotly.newPlot("bar", barArray, barLayout);
-
-
-        // console.log(`result = ${result}`)
-
-
-
-
-
+        //  Create the plot
+        Plotly.newPlot("bar", barArray, barLayout);
     });
 }
+
+
 
 function DrawBubblechart(sampleId)
 {
@@ -61,67 +55,74 @@ function DrawBubblechart(sampleId)
     d3.json(url).then(data => {
         
         let samples = data.samples;
-        let reusltArray = samples.filter(s => s.id == sampleId);
-        let result = resultArray[0]
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
 
         let otu_ids = result.otu_ids;
         let otu_labels = result.otu_labels;
         let sample_values = result.sample_values;
 
-        // create bubble chart
+        // create a trace chart
         let bubbleData = {
             x: otu_ids,
             y: sample_values,
-            text: otu_sabels,
+            text: otu_labels,
             mode: "markers",
             marker: {
-                size: samole_values,
+                size: sample_values,
                 color:otu_ids,
             }
+       
+        };
 
+        // Put the trace into an array
         let bubbleArray = [bubbleData];
 
-        
+        // Create a layout object
+        let bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: {t: 30},
+            hoermode: "closest",
+            xaxis: {title: "OTU ID"}
 
+        };
 
+        // Call Plotly
+        Plotly.newPlot("bubble", bubbleArray, bubbleLayout)
 
-        }
+    });
 
-
-
-    })
-
-
-
-
-
-
-
-
-
-}
-
-function DrawGauge(sampleId)
-{
-    console.log(`DrawGauge(${sampleId})`); 
 }
 
 
 function ShowMetadata(sampleId)
 {
     console.log(`ShowMetadata(${sampleId})`);
+
+    d3.json(url).then(data => {
+        console.log(data);
+    
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+            
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+    
+ 
+        });
+
 }
 
-function optionChanged(sampleId) {
-    console('optionChanged: ${sampleId}')
+
+function optionChanged(sampleId) 
+{
+    console.log(`optionChanged, new value: ${sampleId}`);
 
     DrawBargraph(sampleId);
     DrawBubblechart(sampleId);
     ShowMetadata(sampleId);
-    DrawGauge(sampleId);
-
-
-
 }
 
 function InitDashboard()
@@ -129,42 +130,36 @@ function InitDashboard()
     console.log('InitDashboard()');
 
     // Get a handle to the dropdown
-    let selector = d3.select("#selDataset");
-
-    
+    let selector = d3.select("#selDataset"); 
 
     d3.json(url).then(data => {
         console.log(data);
 
         let sampleNames = data.names;
-        console.log(sampleNames);
+        console.log("Here are the sample names:", sampleNames);
 
-        for (let i = 0; i <sampleNames.length; i++) {
+        for (let i = 0; i < sampleNames.length; i++) {
             let sampleId = sampleNames[i];
-            // console.log(`sampleId = ${sampleId}`);
             selector.append("option").text(sampleId).property("value", sampleId);
 
         };
 
-        let initialID = selector.property("value");
+
+        // Read the current value from the dropdown
+        let initialId = selector.property("value");
         console.log(`initialId = ${initialId}`);
 
+        // Draw the bargraph for the selected sample id
+        DrawBargraph(initialId); 
 
-        DrawBargraph(initialId);
+        // Draw the bubblechart for the selected sample id
+        DrawBubblechart(initialId); 
 
-        Draw
-
-
-
-
-
-
-    })
+        // Draw the metadata for the selected sample id
+        DrawMetadata(initialId); 
 
 
-
-
-
+    });
 
 }
 
